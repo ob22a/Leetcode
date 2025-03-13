cb0ba282f99a416352e8733415d9391e022288c4
Obssa Degefu <Ob22adegefu123@gmail.com>
2025-03-13 23:16:39 +0300

    Time: 1 ms (27.51%) | Memory: 12.6 MB (21.68%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.py
+++ 118-pascals-triangle/pascals-triangle.py
@@ -6,9 +6,10 @@ class Solution(object):
         """
         res=[[1]]
         for i in range(2,numRows+1):
-            val=[1]*i
+            val=[1]
             for j in range(1,i-1):
-                val[j]=res[-1][j-1]+res[-1][j]
+                val.append(res[-1][j-1]+res[-1][j])
+            val.append(1)
             res.append(val)
 
         return res
