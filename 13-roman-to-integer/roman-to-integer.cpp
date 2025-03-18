341767c4786c5c91addfceaa0c07a96a89d0a076
Obssa Degefu <Ob22adegefu123@gmail.com>
2025-03-18 13:04:12 +0300

    Time: 8 ms (39.84%) | Memory: 13.3 MB (35.59%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        std::unordered_map<char,int> values={
+            {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}
+        };
+        int result=0;
+        for(int i=0;i<s.size()-1;i++){
+            if(values[s[i]]<values[s[i+1]]){
+                result-=values[s[i]];
+            }
+            else result+=values[s[i]];
+        }
+        return result +values[s.back()];
+    }
+};
