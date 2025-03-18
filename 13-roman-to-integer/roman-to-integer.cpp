428f3d303d3d7fef56a4d6aed1a5c306bca7603f
Obssa Degefu <Ob22adegefu123@gmail.com>
2025-03-18 13:07:56 +0300

    Time: 16 ms (7.19%) | Memory: 13.3 MB (46.47%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -5,7 +5,7 @@ public:
             {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}
         };
         int result=0;
-        for(int i=0;i<s.size()-1;i++){
+        for(int i=0;i<s.size()-1;++i){
             if(values[s[i]]<values[s[i+1]]){
                 result-=values[s[i]];
             }
