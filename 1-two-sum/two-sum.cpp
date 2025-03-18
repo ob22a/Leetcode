9861d190482386ecf2200c8cba254193e6a4cc08
Obssa Degefu <Ob22adegefu123@gmail.com>
2025-03-18 12:19:25 +0300

    Time: 0 ms (100.00%) | Memory: 14.9 MB (21.26%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,15 +1,11 @@
-#include <vector>
-#include <unordered_map>
-
 class Solution {
 public:
     std::vector<int> twoSum(std::vector<int>& nums, int target) {
-        std::unordered_map<int, int> seen;
+        unordered_map<int, int> seen;
         for (int i = 0; i < nums.size(); ++i) {
             int complement = target - nums[i];
             if (seen.count(complement)) { 
-                std::vector<int> result = {seen[complement], i};
-                return result;
+                return {seen[complement], i};
             }
             seen[nums[i]] = i; 
         }
