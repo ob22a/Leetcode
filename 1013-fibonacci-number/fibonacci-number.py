4fe196aea4d9b42312fccecc31b35272ea0cbe42
Obssa Degefu <Ob22adegefu123@gmail.com>
2025-03-21 20:36:09 +0300

    Time: 41 ms (54.22%) | Memory: 17.8 MB (41.62%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,13 +1,6 @@
-class Solution(object):
-
-    def fib(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        if n<2:
+class Solution:
+    @cache
+    def fib(self, n: int) -> int:
+        if n<=1:
             return n
-        a,b=0,1
-        for i in range(2,n+1):
-            a,b=b,a+b
-        return b 
+        else: return self.fib(n-1)+self.fib(n-2)
