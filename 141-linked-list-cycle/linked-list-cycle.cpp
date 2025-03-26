66639077f2d24419af7cd046774fcefec871317a
Obssa Degefu <Ob22adegefu123@gmail.com>
2025-03-26 20:07:13 +0300

    Time: 8 ms (80.79%) | Memory: 11.8 MB (79.68%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -9,11 +9,13 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        unordered_set<ListNode*> seen;
-        while(head!=nullptr){
-            if(seen.count(head)) return true;
-            seen.insert(head);
-            head=head->next;
+        if(head==nullptr) return false;
+        ListNode* slow=head;
+        ListNode* fast=head->next;
+        while(fast!=nullptr && fast->next!=nullptr){
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast) return true;
         }
         return false;
     }
