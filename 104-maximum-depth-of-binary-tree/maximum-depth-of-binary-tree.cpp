823c278f34540895c8c392345ed20159e9d3f7c3
Obssa Degefu <Ob22adegefu123@gmail.com>
2025-03-27 22:47:45 +0300

    Time: 0 ms (100.00%) | Memory: 19 MB (75.59%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -15,13 +15,16 @@ public:
         if(root==nullptr) return 0;
         vector<tuple<TreeNode*,int>> nodeHeight={{root,1}};
         int height=1;
+
         while(!nodeHeight.empty()){
             tuple<TreeNode*,int> node=nodeHeight.back();
             nodeHeight.pop_back();
+
             if(get<1>(node)>height) height=get<1>(node);
             if(get<0>(node)->left!=nullptr) nodeHeight.push_back({get<0>(node)->left,get<1>(node)+1});
             if(get<0>(node)->right!=nullptr) nodeHeight.push_back({get<0>(node)->right,get<1>(node)+1});
         }
+        
         return height;
     }
 };
