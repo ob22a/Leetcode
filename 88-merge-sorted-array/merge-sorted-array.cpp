c3171f762de3018087c23d6138678a5147551103
Obssa Degefu <Ob22adegefu123@gmail.com>
2025-03-27 21:30:50 +0300

    Time: 0 ms (100.00%) | Memory: 12.2 MB (70.95%) - LeetSync

---
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i=m-1;
+        int j=n-1;
+        while(j>=0){
+            if(i==-1 || nums1[i]<nums2[j]){
+                nums1[i+j+1]=nums2[j];
+                --j;
+            }
+            else{
+                nums1[i+j+1]=nums1[i];
+                --i;
+            }
+        }
+    }
+};
