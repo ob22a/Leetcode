0fcd1ea1be47696ba2859a3c99577c4ce94f6528
Obssa Degefu <Ob22adegefu123@gmail.com>
2025-03-28 08:26:15 +0300

    Time: 0 ms (100.00%) | Memory: 9.8 MB (34.15%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> pascal={{1}};
+        for(int i=2;i<numRows+1;++i){
+            vector<int> row(i,1);
+            for(int j=1;j<i-1;++j){
+                row[j]=pascal.back()[j]+pascal.back()[j-1];
+            }
+            pascal.push_back(row);
+        }
+
+        return pascal;
+    }
+};
