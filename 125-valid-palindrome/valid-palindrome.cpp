2c198bd4b9f2eec7d3822d2b882c5c10bb48c7c4
Obssa Degefu <Ob22adegefu123@gmail.com>
2025-03-28 09:26:12 +0300

    Time: 3 ms (32.14%) | Memory: 9.9 MB (57.62%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int i=0;
+        int j=s.size()-1;
+        while(i<j){
+            if(isalnum(s[i])&&isalnum(s[j])){
+                if(tolower(s[i])!=tolower(s[j])) return false;
+                ++i;
+                --j;
+            }
+            if(!isalnum(s[i])) ++i;
+            if(!isalnum(s[j])) --j;
+        }
+        return true;
+    }
+};
