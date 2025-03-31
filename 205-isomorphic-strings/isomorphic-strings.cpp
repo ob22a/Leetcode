35e329254b77003f5628a33d839318736468a136
Obssa Degefu <Ob22adegefu123@gmail.com>
2025-03-31 09:00:05 +0300

    Time: 0 ms (100.00%) | Memory: 9.2 MB (78.78%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -3,14 +3,12 @@ public:
     bool isIsomorphic(string s, string t) {
         char map1[128]={};
         char map2[128]={};
-        int i=0;
-        while(i<s.size()){//Since s.size()==t.size()
+        for(int i=0;i<s.size();++i){//Since s.size()==t.size()
             if(map1[s[i]]==0 && map2[t[i]]==0){
                 map1[s[i]]=t[i];
                 map2[t[i]]=s[i];
             }
             else if (map1[s[i]]!=t[i] || map2[t[i]]!=s[i]) return false;
-            ++i;
         }
         return true;
     }
