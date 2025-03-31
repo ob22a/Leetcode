cac0cf102263189c3d4f511589f615001495c916
Obssa Degefu <Ob22adegefu123@gmail.com>
2025-03-31 08:28:04 +0300

    Time: 4 ms (5.17%) | Memory: 20.1 MB (73.11%) - LeetSync

---
+++ 203-remove-linked-list-elements/remove-linked-list-elements.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        ListNode* dummy=new ListNode(-1,head);
+        ListNode* current=dummy;
+
+        while(current->next){
+            if(current->next->val==val){
+                ListNode* temp=current->next;
+                current->next=temp->next;
+                delete temp;
+            }
+            else current=current->next;
+        }
+        return dummy->next;
+    }
+};
