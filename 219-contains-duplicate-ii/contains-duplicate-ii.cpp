664a80bf9f71ac461159b9f45b36b83675009034
Obssa Degefu <Ob22adegefu123@gmail.com>
2025-04-01 19:34:33 +0300

    Time: 75 ms (24.59%) | Memory: 98.6 MB (5.80%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -4,9 +4,7 @@ public:
         unordered_map<int,int> numIndex;
 
         for(int i=0;i<nums.size();++i){
-            if(numIndex.find(nums[i])!=numIndex.end()){
-                if(i-numIndex[nums[i]]<=k) return true;
-            }
+            if(numIndex.find(nums[i])!=numIndex.end() && i-numIndex[nums[i]]<=k) return true;
             numIndex[nums[i]]=i;
         }
 
