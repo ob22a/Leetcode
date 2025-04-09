03443e779519f58c7022ea57ac932d4e36b6120d
Obssa Degefu <Ob22adegefu123@gmail.com>
2025-04-09 17:09:26 +0300

    Time: 5 ms (33.90%) | Memory: 8.9 MB (77.76%) - LeetSync

--- 326-power-of-three/power-of-three.cpp
+++ 326-power-of-three/power-of-three.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        if(n==1 || n==3) return true;
-        if(n<=0 || n%3!=0) return false;
-        return isPowerOfThree(n/3);
+        if(n<=0) return false;
+        return 1162261467%n==0;//the maximum power of 3 in int
     }
 };
