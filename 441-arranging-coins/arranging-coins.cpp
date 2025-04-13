9db9dd2000585cd4e8df854b24ce9331aa2c5b7e
Obssa Degefu <Ob22adegefu123@gmail.com>
2025-04-13 23:42:58 +0300

    Time: 0 ms (100.00%) | Memory: 8.9 MB (33.61%) - LeetSync

--- 441-arranging-coins/arranging-coins.cpp
+++ 441-arranging-coins/arranging-coins.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     int arrangeCoins(int n) {
-        if(n==1) return 1;
-        for(long i=1;i<=n;++i){
-            long long total=i*(i+1)/2;
-            long long nextTotal=(i+1)*(i+2)/2;
-            if(total==n || nextTotal>n) return i;
+        int low=0,high=n;
+        while(high>=low){
+            long mid=low+(high-low)/2;
+            long total=mid*(mid+1)/2;
+            if(total==n) return mid;
+            else if(total>n) high=mid-1;
+            else low=mid+1;
         }
-        return -1;
+        return high;
     }
 };
