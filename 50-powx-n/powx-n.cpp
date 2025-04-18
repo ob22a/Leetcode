b44f789a344510bfc34fcf84aba2df1523d76e1c
Obssa Degefu <Ob22adegefu123@gmail.com>
2025-04-18 12:06:45 +0300

    Time: 0 ms (100.00%) | Memory: 8.7 MB (44.79%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    double power(double x, int n){
+        if(n==0) return 1;
+        if(n==1) return x;
+        double half=power(x,n/2);
+        if(n%2==0)  return half*half;
+        return x*half*half;
+    }
+    double myPow(double x, int n) { 
+        long N=n;
+        if(N<0){
+            x=1/x;
+            N=-N;
+        }
+        return power(x,N);
+    }
+};
