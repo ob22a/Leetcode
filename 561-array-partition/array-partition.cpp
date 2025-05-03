e46ece980b679c4b7ffafbb6f5c2d452bd21ff3c
Obssa Degefu <Ob22adegefu123@gmail.com>
2025-05-03 20:54:09 +0300

    Time: 19 ms (14.03%) | Memory: 32.1 MB (17.58%) - LeetSync

--- 561-array-partition/array-partition.cpp
+++ 561-array-partition/array-partition.cpp
@@ -3,7 +3,7 @@ public:
     int arrayPairSum(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         int sol=0;
-        for(int i=0;i<nums.size()-1;i+=2){
+        for(int i=0;i<nums.size();i+=2){
             sol+=nums[i];
         }
         return sol;
