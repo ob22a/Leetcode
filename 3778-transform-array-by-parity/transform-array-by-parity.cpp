20f13ea357dd2de2f36b2a6ea85454e28c6fe262
Obssa Degefu <Ob22adegefu123@gmail.com>
2025-05-06 22:02:20 +0300

    Time: 2 ms (20.52%) | Memory: 28.2 MB (87.67%) - LeetSync

--- 3778-transform-array-by-parity/transform-array-by-parity.cpp
+++ 3778-transform-array-by-parity/transform-array-by-parity.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     vector<int> transformArray(vector<int>& nums) {
-        //Swap even and odd make even come first
+        //Swap even and odd make even come  and set it to 0 and odd 1
         int i=0;
         for(int j=0;j<nums.size();++j){
             if(nums[j]%2==0){
                 swap(nums[i],nums[j]);
+                nums[i]=0;
                 ++i;
             }
-        }
-        //Make even 0 odd 1
-        for(int& num:nums){
-            if(num%2==0) num=0;
-            else num=1;
+            else nums[j]=1;
         }
         return nums;
     }
