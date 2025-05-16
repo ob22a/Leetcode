9d8585d7ded7e20f7947f18a5377e375706e17de
Obssa Degefu <Ob22adegefu123@gmail.com>
2025-05-16 17:46:26 +0300

    Time: 1 ms (39.40%) | Memory: 9.4 MB (74.58%) - LeetSync

---
+++ 316-remove-duplicate-letters/remove-duplicate-letters.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        string sol;
+        unordered_map<char,int> freq;
+        for(char c:s){
+            freq[c]++;
+        }
+        unordered_set<char> seen;
+        for(char c:s){
+            freq[c]--;
+            if(!seen.count(c)){
+                while(!sol.empty() && sol.back()>c && freq[sol.back()]>0){
+                    seen.erase(sol.back());
+                    sol.pop_back();
+                }
+                
+                sol.push_back(c);
+                seen.insert(c);
+            }  
+        }
+        return sol;
+    }
+};
