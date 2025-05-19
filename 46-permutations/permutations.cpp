b19d6e0fbec6737d17445c5db0d35a8b7740b46e
Obssa Degefu <Ob22adegefu123@gmail.com>
2025-05-19 11:37:23 +0300

    Time: 6 ms (9.04%) | Memory: 13.1 MB (7.69%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
-    vector<vector<int>> sol;
-    void backtrack(vector<int> nums,vector<bool> seen,vector<int> res){
+    void backtrack(vector<int> nums,vector<bool> seen,vector<int> res,vector<vector<int>>& sol){
         if(res.size()==nums.size()){
             sol.push_back(res);
             return;
@@ -10,15 +9,16 @@ public:
             if(seen[i]) continue;
             seen[i]=true;
             res.push_back(nums[i]);
-            backtrack(nums,seen,res);
+            backtrack(nums,seen,res,sol);
             res.pop_back();
             seen[i]=false;
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> sol;
         vector<int> res;
         vector<bool> seen(nums.size(),false);
-        backtrack(nums,seen,res);
+        backtrack(nums,seen,res,sol);
         return sol;
     }
 };
