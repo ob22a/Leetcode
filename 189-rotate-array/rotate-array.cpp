834dc4903eeacc83aa434ef3b8c09671c7189905
Obssa Degefu <Ob22adegefu123@gmail.com>
2025-05-22 02:44:33 +0300

    Time: 3 ms (9.74%) | Memory: 29.5 MB (57.33%) - LeetSync

---
+++ 189-rotate-array/rotate-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void reverse(vector<int>& nums,int start,int end){
+        while(start<end) swap(nums[start++],nums[end--]);
+    }
+    void rotate(vector<int>& nums, int k) {
+        if(nums.size()==1 || k%nums.size()==0) return;
+        k=k%nums.size();
+        reverse(nums,0,nums.size()-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k,nums.size()-1);
+    }
+};
