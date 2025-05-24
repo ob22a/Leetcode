84a221c1b01e3782ed9c1ce0a58fe523d10a5378
Obssa Degefu <Ob22adegefu123@gmail.com>
2025-05-24 17:37:30 +0300

    Time: 14 ms (30.26%) | Memory: 19.9 MB (22.54%) - LeetSync

---
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        vector<vector<int>> result;
+        stack<vector<int>> paths;
+        paths.push({0});
+        while(!paths.empty()){
+            vector<int> path=paths.top(); paths.pop();
+            int idx=path.back();
+            if(idx==graph.size()-1) result.push_back(path);
+            else{
+                for(int neighbour:graph[idx]){
+                    path.push_back(neighbour);
+                    paths.push(path);
+                    path.pop_back();
+                }
+            }
+        }
+        return result;
+    }
+};
