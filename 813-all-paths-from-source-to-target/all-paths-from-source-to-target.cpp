51c56eeebd815039a2d65280f8614b24b122b625
Obssa Degefu <Ob22adegefu123@gmail.com>
2025-05-24 17:40:55 +0300

    Time: 17 ms (22.49%) | Memory: 19.8 MB (22.54%) - LeetSync

--- 813-all-paths-from-source-to-target/all-paths-from-source-to-target.cpp
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
         vector<vector<int>> result;
-        stack<vector<int>> paths;
+        queue<vector<int>> paths;
         paths.push({0});
         while(!paths.empty()){
-            vector<int> path=paths.top(); paths.pop();
+            vector<int> path=paths.front(); paths.pop();
             int idx=path.back();
             if(idx==graph.size()-1) result.push_back(path);
             else{
