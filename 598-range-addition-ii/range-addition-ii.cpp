deee8c4997da8572009f037eaa7ec10e9f38124a
Obssa Degefu <Ob22adegefu123@gmail.com>
2025-05-28 10:29:15 +0300

    Time: 0 ms (100.00%) | Memory: 15.4 MB (47.71%) - LeetSync

--- 598-range-addition-ii/range-addition-ii.cpp
+++ 598-range-addition-ii/range-addition-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int maxCount(int m, int n, vector<vector<int>>& ops) {
-        for(vector<int> vec:ops){
+        for(vector<int>& vec:ops){
             m=min(m,vec[0]);
             n=min(vec[1],n);
         }
