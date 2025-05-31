d1ea141d99bdca590e7b21973253c115f5832756
Obssa Degefu <Ob22adegefu123@gmail.com>
2025-05-31 16:43:52 +0300

    Time: 0 ms (100.00%) | Memory: 9.9 MB (45.16%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.cpp
+++ 1127-last-stone-weight/last-stone-weight.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     int lastStoneWeight(vector<int>& stones) {
-        priority_queue<int> maxHeap;
-        for(int weight:stones) maxHeap.push(weight);
+        priority_queue<int> maxHeap(stones.begin(),stones.end());
         while(maxHeap.size()>1){
             int max1=maxHeap.top(); maxHeap.pop();
             int max2=maxHeap.top(); maxHeap.pop();
