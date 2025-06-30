e6f829e2e690421b448451afaf5f9ad9f0b66e13
Obssa Degefu <Ob22adegefu123@gmail.com>
2025-06-30 16:46:27 +0300

    Time: 0 ms (100.00%) | Memory: 11.2 MB (59.86%) - LeetSync

--- 2791-find-the-losers-of-the-circular-game/find-the-losers-of-the-circular-game.cpp
+++ 2791-find-the-losers-of-the-circular-game/find-the-losers-of-the-circular-game.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     vector<int> circularGameLosers(int n, int k) {
-        int players[50]={};
+        bool players[50]={};
         int current=0,pos=1;
-        while(++players[current]<=1){
+        while(!players[current]){
+            players[current]=true;
             current=(current+(pos*k))%n;
             ++pos;
         }
         vector<int>sol;
         for(int i=0;i<n;++i){
-            if(players[i]==0) sol.emplace_back(i+1);
+            if(!players[i]) sol.emplace_back(i+1);
         }
         return sol;
     }
