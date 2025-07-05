8633f8c0945551fbfac77478cde2bee4d6cd47b8
Obssa Degefu <Ob22adegefu123@gmail.com>
2025-07-05 19:42:06 +0300

    Time: 4 ms (21.84%) | Memory: 41.2 MB (40.46%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> sol(nums.size());
+        vector<int>rpp(nums.size()); // REVERSE PREFIX PRODUCT 
+        for(int i=nums.size()-1;i>=0;--i){
+            if(i==nums.size()-1) rpp[i]=nums[i];
+            else rpp[i]=rpp[i+1]*nums[i];
+        }
+
+        int preProduct=1;
+        for(int i=0;i<nums.size();++i){
+            sol[i]=(i!=nums.size()-1)?rpp[i+1]*preProduct:preProduct;
+            preProduct*=nums[i];
+        }
+        return sol;
+    }
+};
