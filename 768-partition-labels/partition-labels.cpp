da89e7fb44a96b6c2156f2fcd4da7c8c2e8b1223
Obssa Degefu <Ob22adegefu123@gmail.com>
2025-07-06 12:05:43 +0300

    Time: 3 ms (28.83%) | Memory: 8.7 MB (96.97%) - LeetSync

--- 768-partition-labels/partition-labels.cpp
+++ 768-partition-labels/partition-labels.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     vector<int> partitionLabels(string s) {
-        unordered_map<char,int> pos;
-        for(int idx=0;idx<s.size();++idx) pos[s[idx]]=idx; 
+        int pos[26]={};
+        for(int idx=0;idx<s.size();++idx) pos[s[idx]-'a']=idx; 
         // We got the last index of character
         
         vector<int>sol;
-        int start=0,end=pos[s[start]];
+        int start=0,end=pos[s[start]-'a'];
         for(int i=0;i<s.size();++i){
-            end=max(end,pos[s[i]]);
+            end=max(end,pos[s[i]-'a']);
             if(i==end){
                 sol.push_back(end-start+1);
                 start=end+1;
