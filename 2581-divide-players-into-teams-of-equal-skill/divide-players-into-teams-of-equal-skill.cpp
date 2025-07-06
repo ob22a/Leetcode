356c295b8761db0a97018e7f821473d7a0599ae7
Obssa Degefu <Ob22adegefu123@gmail.com>
2025-07-06 09:55:47 +0300

    Time: 19 ms (25.44%) | Memory: 56.3 MB (82.99%) - LeetSync

---
+++ 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    long long dividePlayers(vector<int>& skill) {
+        int n=skill.size();
+        if(n==2) return 1ll*skill[0]*skill[1];
+        int total=0;
+        for(int num:skill) total+=num;
+
+        sort(skill.begin(),skill.end());
+        
+        int l=0,r=n-1,no=n/2;
+        long long sol=0;
+        if(total%no!=0) return -1;
+        while(l<r){
+            if(skill[l]+skill[r]==total/no) sol+=((long long)skill[l]*skill[r]);
+            else return -1;
+            ++l; --r;
+        }
+        return sol;
+    }
+};
