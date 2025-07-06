13f93f0e3b30572e844623c1c41a9f8e7d6551bd
Obssa Degefu <Ob22adegefu123@gmail.com>
2025-07-06 16:06:29 +0300

    Time: 3 ms (88.72%) | Memory: 74.8 MB (28.95%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
-        for(int& num:nums){
-            if(num%2==0) num=0;
-            else num=1;
-        }
         vector<int> preCount(nums.size()+1);
         preCount[0]=1;
         int sum=0,count=0;
         for(int num:nums){
-            sum+=num;
+            sum+=(num%2==0)?0:1;
             if(sum-k>=0){
                 count+=preCount[sum-k];
             }
