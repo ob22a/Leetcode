1d303c26a375cd91ce6886a269ec24e059ab75e1
Obssa Degefu <Ob22adegefu123@gmail.com>
2025-07-27 13:52:39 +0300

    Time: 7 ms (43.24%) | Memory: 102 MB (65.42%) - LeetSync

--- 463-island-perimeter/island-perimeter.cpp
+++ 463-island-perimeter/island-perimeter.cpp
@@ -1,32 +1,18 @@
 class Solution {
 public:
-    int dfs(vector<vector<int>>& grid,vector<vector<bool>>& seen,int i,int j){
-        if(i<0 || i>=grid.size() || j<0 || j>=grid[0].size() || grid[i][j]==0 || seen[i][j]) return 0;
-        seen[i][j]=true;
-        int sum=4;
-
-        if (i > 0 && grid[i - 1][j] == 1) sum -= 1;
-        if (i < grid.size() - 1 && grid[i + 1][j] == 1) sum -= 1;
-        if (j > 0 && grid[i][j - 1] == 1) sum -= 1;
-        if (j < grid[0].size() - 1 && grid[i][j + 1] == 1) sum -= 1;
-
-        sum+=dfs(grid,seen,i,j+1);
-        sum+=dfs(grid,seen,i+1,j);
-        sum+=dfs(grid,seen,i,j-1);
-        sum+=dfs(grid,seen,i-1,j);
-
-        return sum;
-    }
     int islandPerimeter(vector<vector<int>>& grid) {
         int n=grid.size(),m=grid[0].size();
-        vector<vector<bool>> seen(n,vector<bool>(m,false));
+        int sol=0;
         for(int i=0;i<n;++i){
             for(int j=0;j<m;++j){
-                if(grid[i][j]==1){
-                    return dfs(grid,seen,i,j);
+                if(grid[i][j]){
+                    if(i==0 || !grid[i-1][j]) sol++;
+                    if(i==n-1 || !grid[i+1][j]) sol++;
+                    if(j==0 || !grid[i][j-1]) sol++;
+                    if(j==m-1 || !grid[i][j+1]) sol++;
                 }
             }
         }
-        return 0;
+        return sol;
     }
 };
