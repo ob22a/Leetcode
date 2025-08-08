8174712db4326b57ad43edce3bd072a7b40b13eb
Obssa Degefu <Ob22adegefu123@gmail.com>
2025-08-08 13:41:23 +0300

    Time: 0 ms (100.00%) | Memory: 7.7 MB (98.22%) - LeetSync

---
+++ 1033-broken-calculator/broken-calculator.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int brokenCalc(int startValue, int target) {
+        int count=0;
+        while(target>startValue){
+            if(target%2!=0) ++target;
+            else target/=2;
+            ++count;
+        }
+        
+        return count+(startValue-target);
+    }
+};
