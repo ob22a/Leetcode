132646f34348b4e850464ade661b7af7b959c5d2
Obssa Degefu <Ob22adegefu123@gmail.com>
2025-08-28 13:37:42 +0300

    Time: 0 ms (100.00%) | Memory: 21.4 MB (51.67%) - LeetSync

--- 950-x-of-a-kind-in-a-deck-of-cards/x-of-a-kind-in-a-deck-of-cards.cpp
+++ 950-x-of-a-kind-in-a-deck-of-cards/x-of-a-kind-in-a-deck-of-cards.cpp
@@ -3,7 +3,7 @@ public:
     bool hasGroupsSizeX(vector<int>& deck) {
         unordered_map<int,int>freq;
         for(int num:deck) freq[num]++;
-        int x=freq[deck[0]];
+        int x=0;
         for(const auto& p:freq){
             x=__gcd(x,p.second);
             if(x==1) return false;
