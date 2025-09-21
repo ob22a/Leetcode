1d2b29dac10db7afb2f869c8bd5072fd55938ec5
Obssa Degefu <Ob22adegefu123@gmail.com>
2025-09-21 16:13:50 +0300

    Time: 35 ms (55.93%) | Memory: 21.9 MB (55.32%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -2,23 +2,26 @@ class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
-        vector<vector<int>> dp(n,vector<int>(amount+1,INT_MAX)); //idx * sum+1
-        for(int i=0;i<n;++i) dp[i][0]=0;
+
+        vector<int> dp(amount+1,INT_MAX);  dp[0]=0;
+        vector<int> temp(amount+1,INT_MAX); temp[0]=0;
+
         for(int j=0;j<=amount;++j){
-            if(j%coins[0]==0) dp[0][j]=j/coins[0];
+            if(j%coins[0]==0) dp[j]=j/coins[0];
         }
 
         for(int idx=1;idx<n;++idx){
             for(int sum=1;sum<=amount;++sum){
-                long long notTake = dp[idx-1][sum];
+                long long notTake = dp[sum];
                 long long take = INT_MAX;
 
-                if(sum>=coins[idx]) take = 1ll + dp[idx][sum-coins[idx]];
-                dp[idx][sum]=min(take,notTake);
+                if(sum>=coins[idx]) take = 1ll + temp[sum-coins[idx]];
+                temp[sum]=min(take,notTake);
             }
+            dp=temp;
         }
         
-        int sol = dp[n-1][amount];
+        int sol = dp[amount];
         if(sol==INT_MAX) sol=-1;
 
         return sol;
