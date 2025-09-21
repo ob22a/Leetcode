c33e8cf2cb013d9e2f5bb837e98fc5ddb9e99d29
Obssa Degefu <Ob22adegefu123@gmail.com>
2025-09-21 16:18:17 +0300

    Time: 23 ms (71.57%) | Memory: 18 MB (73.76%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -2,9 +2,7 @@ class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
-
         vector<int> dp(amount+1,INT_MAX);  dp[0]=0;
-        vector<int> temp(amount+1,INT_MAX); temp[0]=0;
 
         for(int j=0;j<=amount;++j){
             if(j%coins[0]==0) dp[j]=j/coins[0];
@@ -15,10 +13,9 @@ public:
                 long long notTake = dp[sum];
                 long long take = INT_MAX;
 
-                if(sum>=coins[idx]) take = 1ll + temp[sum-coins[idx]];
-                temp[sum]=min(take,notTake);
+                if(sum>=coins[idx]) take = 1ll + dp[sum-coins[idx]];
+                dp[sum]=min(take,notTake);
             }
-            dp=temp;
         }
         
         int sol = dp[amount];
