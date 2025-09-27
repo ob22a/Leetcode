a5cbb30aef64a76c9f549498fd952593a61f9194
Obssa Degefu <Ob22adegefu123@gmail.com>
2025-09-27 22:53:59 +0300

    Time: 123 ms (44.28%) | Memory: 14.5 MB (50.62%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -12,19 +12,16 @@ public:
     int lengthOfLIS(vector<int>& nums) {
         // Find some function f(i) where this would return the longest subsequnce
         int n = nums.size();
-
         vector<int> dp(n+1,0);
-        vector<int> temp(n+1,0);
 
         for(int idx=n-1;idx>=0;--idx){
             for(int prevIdx=idx-1;prevIdx>=-1;--prevIdx){
-                int notPick = temp[prevIdx+1];
+                int notPick = dp[prevIdx+1];
                 int pick = 0;
-                if(prevIdx==-1 || nums[idx]>nums[prevIdx]) pick = 1 + temp[idx+1];
+                if(prevIdx==-1 || nums[idx]>nums[prevIdx]) pick = 1 + dp[idx+1];
 
                 dp[prevIdx+1] = max(pick,notPick);
             }
-            temp = dp;
         }
         return dp[0];
     }
