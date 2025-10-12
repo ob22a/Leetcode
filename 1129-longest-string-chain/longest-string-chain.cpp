876ca5d81ac07d07814af02435fcc8779bf9f24f
Obssa Degefu <Ob22adegefu123@gmail.com>
2025-10-12 19:03:50 +0300

    Time: 1812 ms (22.20%) | Memory: 651.3 MB (5.62%) - LeetSync

--- 1129-longest-string-chain/longest-string-chain.cpp
+++ 1129-longest-string-chain/longest-string-chain.cpp
@@ -25,8 +25,8 @@ public:
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < i; ++j) {
                 if (valid(words[i], words[j])) dp[i]=max(dp[i], dp[j] + 1);
-                maxLen = max(maxLen, dp[i]);
             }
+            maxLen = max(maxLen, dp[i]);
         }
         return maxLen;
     }
