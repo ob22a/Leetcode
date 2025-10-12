8258dc0a28993c38a6ad148f718939e3714593b4
Obssa Degefu <Ob22adegefu123@gmail.com>
2025-10-12 19:02:54 +0300

    Time: 1795 ms (23.45%) | Memory: 651.3 MB (5.62%) - LeetSync

---
+++ 1129-longest-string-chain/longest-string-chain.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool valid(string longer, string shorter){
+        if(longer.size()!=shorter.size()+1) return false;
+        int i=0,j=0;
+        while(i<longer.size()){
+            if(longer[i]==shorter[j]){
+                ++i; ++j;
+            } else ++i;
+        }
+
+        return j==shorter.size();
+    }
+
+    static bool comp(string word1,string word2){
+        return word1.size()<word2.size();
+    }
+    int longestStrChain(vector<string>& words) {
+        int n = words.size();
+        vector<int> dp(n,1);
+        sort(words.begin(),words.end(),comp);
+
+        int maxLen = 1;
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (valid(words[i], words[j])) dp[i]=max(dp[i], dp[j] + 1);
+                maxLen = max(maxLen, dp[i]);
+            }
+        }
+        return maxLen;
+    }
+};
