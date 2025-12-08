d9267524ae686dbe331e41b10611d0519fe283bc
Obssa Degefu <Ob22adegefu123@gmail.com>
2025-12-08 11:56:35 +0300

    Time: 10 ms (47.09%) | Memory: 94.4 MB (14.56%) - LeetSync

--- 2205-find-good-days-to-rob-the-bank/find-good-days-to-rob-the-bank.cpp
+++ 2205-find-good-days-to-rob-the-bank/find-good-days-to-rob-the-bank.cpp
@@ -7,13 +7,11 @@ public:
         vector<int> dec(n,0);
 
         for(int i=0;i<n;++i){
-            if(i==0 || security[i-1]<security[i]) continue;
-            dec[i]=1+dec[i-1];
+            if(i!=0 && security[i-1]>=security[i]) dec[i]=1+dec[i-1];
         }
 
         for(int i=n-1;i>=0;--i){
-            if(i==n-1 || security[i+1]<security[i]) continue;
-            inc[i]=1+inc[i+1];
+            if(i!=n-1 && security[i+1]>=security[i]) inc[i]=1+inc[i+1];
         }
 
         vector<int> sol;
