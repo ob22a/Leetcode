941c8fe014be1a078be1c55837622fac73e79088
Obssa Degefu <Ob22adegefu123@gmail.com>
2026-01-19 22:05:13 +0300

    Time: 94 ms (75.83%) | Memory: 13.8 MB (57.01%) - LeetSync

---
+++ 312-burst-balloons/burst-balloons.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int f(vector<int>& nums, int i, int j){
+        if(j<i) return 0;
+
+        int maxi = INT_MIN;
+        for(int k=i;k<=j;++k){
+            int cost = nums[i-1]*nums[k]*nums[j+1] + f(nums,i,k-1) + f(nums,k+1,j);
+            maxi = max(cost,maxi);
+        }
+
+        return maxi;
+    }
+    int maxCoins(vector<int>& nums) {
+        nums.insert(nums.begin(),1);
+        nums.push_back(1);
+
+        int n = nums.size();
+
+        vector<vector<int>> dp(n,vector<int>(n,0));
+        for(int i=n-2;i>=1;--i){
+            for(int j=i;j<n-1;++j){
+                int maxi = INT_MIN;
+                for(int k=i;k<=j;++k){
+                    int cost = nums[i-1]*nums[k]*nums[j+1] + dp[i][k-1] + dp[k+1][j];
+                    maxi = max(cost,maxi);
+                }
+                dp[i][j]=maxi;
+            }
+        }
+
+        return dp[1][n-2];
+    }
+};
