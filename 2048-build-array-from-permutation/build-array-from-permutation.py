7c9ca306441bf94bc13fbdc905adeaa7b092b9db
Obssa Degefu <Ob22adegefu123@gmail.com>
2026-01-27 21:51:40 +0300

    Time: 0 ms (100.00%) | Memory: 19.4 MB (39.66%) - LeetSync

--- 2048-build-array-from-permutation/build-array-from-permutation.py
+++ 2048-build-array-from-permutation/build-array-from-permutation.py
@@ -1,6 +1,6 @@
 class Solution:
     def buildArray(self, nums: List[int]) -> List[int]:
-        arr = copy.deepcopy(nums)
+        arr = nums[:]
         n = len(nums)
 
         for idx in range(n):
