ec4d42dbf7f599e7bcca8285a6f34962020c6d41
Obssa Degefu <Ob22adegefu123@gmail.com>
2026-02-11 23:23:35 +0300

    Time: 3 ms (32.05%) | Memory: 19.5 MB (14.64%) - LeetSync

--- 1651-shuffle-string/shuffle-string.py
+++ 1651-shuffle-string/shuffle-string.py
@@ -1,7 +1,6 @@
 class Solution:
     def restoreString(self, s: str, indices: List[int]) -> str:
         sol = list(s)
-        print(sol)
         for i in range(len(indices)):
             sol[indices[i]]= s[i]
         
