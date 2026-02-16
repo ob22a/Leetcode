d6dfd737dc5357e2ed73aa2dfc8977f93660c06f
Obssa Degefu <Ob22adegefu123@gmail.com>
2026-02-16 08:17:43 +0300

    Time: 33 ms (61.56%) | Memory: 19.3 MB (67.09%) - LeetSync

--- 3193-maximum-strong-pair-xor-i/maximum-strong-pair-xor-i.py
+++ 3193-maximum-strong-pair-xor-i/maximum-strong-pair-xor-i.py
@@ -1,10 +1,12 @@
 class Solution:
     def maximumStrongPairXor(self, nums: List[int]) -> int:
         # diff is smaller than min
+        n = len(nums)
         sol = 0
-        for num1 in nums:
-            for num2 in nums:
-                if abs(num1-num2)<=min(num1,num2):
-                    sol = max(sol,num1^num2)
+        
+        for idx1 in range(n):
+            for idx2 in range(idx1,n):
+                if abs(nums[idx1]-nums[idx2])<=min(nums[idx1],nums[idx2]):
+                    sol = max(sol,nums[idx1]^nums[idx2])
 
         return sol
