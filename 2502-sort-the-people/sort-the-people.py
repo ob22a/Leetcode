42fb9c80ce09732b42e88e5cdcf547fba04bbd48
Obssa Degefu <Ob22adegefu123@gmail.com>
2026-02-24 21:07:49 +0300

    Time: 834 ms (5.02%) | Memory: 19.7 MB (46.64%) - LeetSync

---
+++ 2502-sort-the-people/sort-the-people.py
@@ -0,0 +1,15 @@
+class Solution:
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+
+        n = len(heights)
+        for i in range(n):
+            is_swapped = False
+            for j in range(0,n-i-1):
+                if heights[j]<heights[j+1]:
+                    is_swapped=True
+                    heights[j],heights[j+1]=heights[j+1],heights[j]
+                    names[j],names[j+1]=names[j+1],names[j]
+        
+            if not is_swapped: break
+        
+        return names
