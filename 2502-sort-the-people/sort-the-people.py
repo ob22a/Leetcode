34e6de8eef34810c5571b3ff278d2c85ec016f31
Obssa Degefu <Ob22adegefu123@gmail.com>
2026-02-25 15:32:45 +0300

    Time: 22 ms (35.63%) | Memory: 19.9 MB (19.00%) - LeetSync

--- 2502-sort-the-people/sort-the-people.py
+++ 2502-sort-the-people/sort-the-people.py
@@ -1,38 +1,25 @@
 class Solution:
     def sortPeople(self, names, heights):
-        #using merge sort 
-        def merge(a,b):
-            c=[]
-            n1=len(a)
-            n2=len(b)
+        # Quick sort 
+        def quickSort(arr,left,right):
+            if left>=right:
+                return
 
-            i=0
-            j=0
-            while i<n1 and j<n2:
-                if a[i]<b[j]:
-                    c.append(a[i])
+            pivot = right-1 # Assume pivot is always at the end
+            i = left
+            for j in range(left,right):
+                if arr[j]>arr[pivot]:
+                    arr[i],arr[j]=arr[j],arr[i]
                     i+=1
-                else:
-                    c.append(b[j])
-                    j+=1
+            arr[i],arr[pivot]=arr[pivot],arr[i]
             
-            if i<n1: 
-                for idx in range(i,n1):
-                    c.append(a[idx])
-            elif j<n2:
-                for idx in range(j,n2):
-                    c.append(b[idx])
-
-            return c
+            quickSort(arr,left,i)
+            quickSort(arr,i+1,right)
         
-        def mergeSort(a):
-            n = len(a)
-            if n<=1:
-                return a
-            return merge(mergeSort(a[:n//2]),mergeSort(a[n//2:]))
-            
         n = len(heights)
         array = [(heights[i],names[i]) for i in range(n)]
-        array = mergeSort(array)
 
-        return [name for _,name in array[::-1]]        
+        quickSort(array,0,n)
+        #print(array)
+
+        return [name for _,name in array]
