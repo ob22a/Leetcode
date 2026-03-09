a6ee508887acedac89970ba4c6e0faf9b4fafd90
Obssa Degefu <Ob22adegefu123@gmail.com>
2026-03-09 21:20:30 +0300

    Time: 10 ms (38.24%) | Memory: 19.6 MB (92.58%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,21 +1,20 @@
-class Solution(object):
-    def isPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        if len(s.strip())<=1:
-            return True
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
         l=0
         r=len(s)-1
+
         while l<r:
-            if s[l].isalnum() and s[r].isalnum():
-                if s[l].lower()!=s[r].lower():
-                    return False
+            if not s[l].isalpha() and not s[l].isdigit():
                 l+=1
+                continue
+            if not s[r].isalpha() and not s[r].isdigit():
                 r-=1
-            if not s[l].isalnum():
-                l+=1
-            if not s[r].isalnum():
-                r-=1
+                continue
+
+            if s[l].lower()!=s[r].lower():
+                return False
+            
+            l+=1
+            r-=1
+        
         return True
