6d5c17eded0e337a8aa03b56a4bf2b14adf2ee7b
Obssa Degefu <Ob22adegefu123@gmail.com>
2026-04-04 23:22:21 +0300

    Time: 11 ms (15.95%) | Memory: 20.2 MB (40.02%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -4,18 +4,21 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
         m,n=len(matrix),len(matrix[0])
-        
-        zeroRow = set()
-        zeroCol = set()
+
+        zeroFirstCol = False
 
         for i in range(m):
-            for j in range(n):
+            if matrix[i][0]==0:
+                zeroFirstCol = True
+
+            for j in range(1,n):
                 if matrix[i][j]==0:
-                    zeroRow.add(i)
-                    zeroCol.add(j)
-        
-        for i in range(m):
-            for j in range(n):
-                if i in zeroRow or j in zeroCol:
-                    matrix[i][j]=0
-        
+                    matrix[0][j]=0
+                    matrix[i][0]=0
+
+        for row in range(m-1,-1,-1):
+            for col in range(n-1,0,-1):
+                if matrix[row][0]==0 or matrix[0][col]==0:
+                    matrix[row][col]=0
+            if zeroFirstCol:
+                matrix[row][0]=0
