347af7600d49c6f4a58ef646786d657708ae5fac
Obssa Degefu <Ob22adegefu123@gmail.com>
2026-04-09 20:56:08 +0300

    Time: 137 ms (29.82%) | Memory: 30.5 MB (87.77%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
@@ -1,18 +1,21 @@
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
         n = len(heights)
-        stk = [] #idx height
-        area = 0
+        stk = [] # Monotonic increassing stack holding val idx
+        sol = 0
 
         for i in range(n+1):
-            start = i
-            h = heights[i] if i!=n else 0 # To collect final area from the heights remaining in the stack
-            while stk and h<stk[-1][1]:
-                height = stk[-1][1]
-                width = i-stk[-1][0]
-                area = max(area,height*width)
-                start = stk[-1][0]
+            start=i # Rectangle with height heights[i] starts at this point
+            h = heights[i] if i!=n else 0
+            while stk and stk[-1][0]>=h:
+                height = stk[-1][0]
+                width = i - stk[-1][1]
+                sol = max(sol,height*width)
+                start = stk[-1][1]
+
                 stk.pop()
-            stk.append((start,h))
+            
+            if i!=n: 
+                stk.append((heights[i],start))
         
-        return area
+        return sol
