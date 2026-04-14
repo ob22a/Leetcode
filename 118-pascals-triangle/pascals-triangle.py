3886de7a04cb9a33637de2616ca41b4c9965c9d2
Obssa Degefu <Ob22adegefu123@gmail.com>
2026-04-14 20:45:19 +0300

    Time: 0 ms (100.00%) | Memory: 19.4 MB (13.24%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.py
+++ 118-pascals-triangle/pascals-triangle.py
@@ -1,14 +1,13 @@
-class Solution(object):
-    def generate(self, numRows):
-        """
-        :type numRows: int
-        :rtype: List[List[int]]
-        """
-        res=[[1]]
-        for i in range(2,numRows+1):
-            val=[1]*i
-            for j in range(1,i-1):
-                val[j]=res[-1][j-1]+res[-1][j]
-            res.append(val)
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        sol = []
 
-        return res
+        for row in range(numRows):
+            newRow = [1]*(row+1)
+            
+            for idx in range(1,row):
+                newRow[idx]=sol[row-1][idx]+sol[row-1][idx-1]
+            
+            sol.append(newRow)
+        
+        return sol
