972bebe38369ccbe5056ef20c98d4164bcec0aec
Obssa Degefu <Ob22adegefu123@gmail.com>
2026-05-05 23:29:59 +0300

    Time: 35 ms (15.90%) | Memory: 20.7 MB (47.07%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.py
@@ -0,0 +1,17 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        ans = 0
+
+        for i in range(32):
+            sum=0
+            for num in nums:
+                if num<0:
+                    num=num& 0xFFFFFFFF
+                sum+=num>>i & 1
+            sum%=3
+            ans|=sum<<i
+        
+        if ans>=2**31:
+            ans-=2**32
+            
+        return ans
