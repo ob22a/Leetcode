34bb315b6d7f3efa4a67e78e7d4910a95fc95770
Obssa Degefu <Ob22adegefu123@gmail.com>
2026-07-01 18:45:15 +0300

    Time: 529 ms (16.85%) | Memory: 24.9 MB (12.00%) - LeetSync

--- 127-word-ladder/word-ladder.py
+++ 127-word-ladder/word-ladder.py
@@ -1,20 +1,25 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        graph = defaultdict(list)
-        if beginWord not in wordList:
-            wordList.insert(0,beginWord)
+        if endWord not in wordList:
+            return 0
 
         n = len(wordList)
-
         word_set = set(wordList)
-        
-        for word in wordList:
+        graph = defaultdict(list)
+
+        def next_words(word)->list:
+            if word in graph:
+                return graph[word]
+
+            words = []
             for i in range(len(word)):
                 for c in 'abcdefghijklmnopqrstuvwxyz':
                     new_word = word[:i] + c + word[i+1:]
                     if new_word in word_set:
-                        graph[word].append(new_word)
-                        graph[new_word].append(word)
+                        words.append(new_word)
+
+            graph[word]=words
+            return words
 
         # for i in range(n):
         #     for j in range(i+1,n):
@@ -25,24 +30,20 @@ class Solution:
         #print(graph)
         # Traverse graph and find cost (cost,word)
 
-        visited = set()
+        visited = {beginWord}
         q = deque([(1,beginWord)])
 
         while q:
             #print(q)
             c,w = q.popleft()
             
-            if w in visited:
-                continue
-            
             if w==endWord:
                 return c
-            
-            visited.add(w)
 
-            for word in graph[w]:
+            for word in next_words(w):
                 if word not in visited:
                     q.append((c+1,word))
+                    visited.add(w)
             
         return 0
 
